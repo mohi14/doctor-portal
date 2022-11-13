9d2bee229af5dcc7b7f72399bd7ea1cdfb0cb99b
mohi14 <mohiuddinkhan1409@gmail.com>
2022-11-13 23:07:02 +0600

    login and signup page done

--- src/Pages/Routes/Routes/Routes.js
+++ src/Pages/Routes/Routes/Routes.js
@@ -3,6 +3,7 @@ import Main from "../../../layout/Main";
 import Appointment from "../../Appointment/Appointment/Appointment";
 import Home from "../../Home/Home/Home";
 import Login from "../../Login/Login";
+import SignUp from "../../SignUp/SignUp";
 
 
 export const router = createBrowserRouter([
@@ -21,6 +22,10 @@ export const router = createBrowserRouter([
             {
                 path: '/appointment',
                 element: <Appointment></Appointment>
+            },
+            {
+                path: '/signup',
+                element: <SignUp></SignUp>
             }
         ]
     }
--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,18 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
-const Login = () => {
+const SignUp = () => {
     const { register, formState: { errors }, handleSubmit } = useForm();
-    const handleLogin = data => {
+    const handleSignUp = data => {
         console.log(data)
     }
     return (
         <div className='h-[800px] flex justify-center items-center'>
             <div className='w-96 p-7'>
-                <h2 className='text-xl text-center'>Login</h2>
-                <form onSubmit={handleSubmit(handleLogin)}>
-
+                <h2 className='text-xl text-center'>Signup</h2>
+                <form onSubmit={handleSubmit(handleSignUp)}>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"><span className="label-text">Name</span></label>
+                        <input type="text" {...register("name", { required: "Name is required" })} className="input input-bordered w-full max-w-xs" />
+                        {errors.name && <p role="alert" className='text-red-600'>{errors.name?.message}</p>}
+                    </div>
                     <div className="form-control w-full max-w-xs">
                         <label className="label"><span className="label-text">Email</span></label>
                         <input type="email" {...register("email", { required: "Email Address is required" })} className="input input-bordered w-full max-w-xs" />
@@ -23,14 +27,14 @@ const Login = () => {
                         <input type="password" {...register("password",
                             {
                                 required: 'Password is required',
-                                minLength: { value: 6, message: 'Password must be 6 character or longer' }
+                                minLength: { value: 6, message: 'Password must be 6 character or longer' },
+                                pattern: { value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/, message: 'Password must be strong' }
                             })} className="input input-bordered w-full max-w-xs" />
-                        <label className="label"><span className="label-text">Forget Password?</span></label>
                         {errors.password && <p role="alert" className='text-red-600'>{errors.password?.message}</p>}
                     </div>
-                    <input className='btn btn-accent w-full' value='Login' type="submit" />
+                    <input className='btn btn-accent w-full mt-4' value='Signup' type="submit" />
                 </form>
-                <p>New to Doctor Portal? <Link className='text-secondary' to='/signup'>Create New Account.</Link></p>
+                <p>Already Have an Account? <Link className='text-secondary' to='/login'>Please Login.</Link></p>
                 <div className="divider">OR</div>
                 <button className='btn btn-outline w-full'>CONTINUE WITH GOOGLE</button>
             </div>
@@ -38,4 +42,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default SignUp;
